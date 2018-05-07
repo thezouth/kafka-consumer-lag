2e2646cabe7c1ad5b24cf8641cfcfebe33bb2576
Kajornthep Piyanun <kachontep@gmail.com>
2018-05-07 21:36:37 +0700

    Support elasticsearch-url and daily indices generated feature

--- kafka-consumer-lag.sh
+++ kafka-consumer-lag.sh
@@ -41,21 +41,40 @@ checkConsumerGroupOffset() {
     CONSUMER_GROUP=$1
 
     SED_EXPR='3,$p'
-    AWK_EXPR='BEGIN { printf "["; } { for(i = 1; i < NF; i++) { printf "\"%s\",", $i; } printf "\"%s\"", $i; } END { printf "]\n"; }'
-    JQ_EXPR="{consumerGroup: \"$CONSUMER_GROUP\", consumerId: .[5], host: .[6], clientId: .[7], topic: .[0], partition: .[1]|tonumber, currentOffset: .[2]|tonumber, logEndOffset: .[3]|tonumber, lag: .[4]|tonumber}"
+
+    AWK_EXPR='BEGIN { printf "["; } '
+    AWK_EXPR+='{ for(i = 1; i < NF; i++) { printf "\"%s\",", $i; } printf "\"%s\"", $i; } '
+    AWK_EXPR+='END { printf "]\n"; }'
+
+    INDEX_DOC="kafka-consumer-lag-$(date +'%Y.%m.%d')"
+    INDEX_TIME="$(date +'%Y/%m/%d %T')"
+
+    JQ_EXPR="{"
+    JQ_EXPR+="consumer_group: \"$CONSUMER_GROUP\", "
+    JQ_EXPR+="consumer_id: .[5], host: .[6], client_id: .[7], topic: .[0], partition: .[1]|tonumber, "
+    JQ_EXPR+="current_offset: .[2]|tonumber, log_end_offset: .[3]|tonumber, lag: .[4]|tonumber, "
+    JQ_EXPR+="\"@timestamp\": \"${INDEX_TIME}\""
+    JQ_EXPR+="}"
 
     KAFKA_CMD="$CONSUMER_GROUP_CMD --bootstrap-server $BOOTSTRAP_SERVER --describe --group $CONSUMER_GROUP"
-    OUTPUT=$($KAFKA_CMD 2>/dev/null)
+    KAFKA_OUTPUT=$($KAFKA_CMD 2>/dev/null)
 
-    if [[ "$OUTPUT" =~ ^(E|e)rror ]]; then
-        echo "$OUTPUT"
+    if [[ "$KAFKA_OUTPUT" =~ ^(E|e)rror ]]; then
+        echo "$KAFKA_OUTPUT"
     else
-        echo "$OUTPUT" \
-            | sed -n "${SED_EXPR}" \
-            | awk "${AWK_EXPR}" \
-            | jq "${JQ_EXPR}"
+        ES_DOC=$(echo "$KAFKA_OUTPUT" \
+                | sed -n "${SED_EXPR}" \
+                | awk "${AWK_EXPR}" \
+                | jq -c "${JQ_EXPR}"
+            )
+
+        if [ ! -z "$ES_URL" ]; then
+            curl -X POST -H 'Content-Type: application/json' -d "$ES_DOC" $ES_URL/$INDEX_DOC/_doc
+        fi
     fi
 }
+
+##----------------------------------------------------------------------##
 
 #
 # Parse command line arguments
@@ -71,11 +90,11 @@ while [[ $# -gt 0 ]]; do
         shift
         ;;
         -O|--elasticsearch-url)
-        output="$2"
+        ES_URL="$2"
         shift
         shift
         ;;
-        -t|--interval)
+        -T|--interval)
         INTERVAL="$2"
         shift
         shift
@@ -97,17 +116,25 @@ if [ -z "$BOOTSTRAP_SERVER" ]; then
     exit 0
 fi
 
-# if [ -z "$output" ]; then
-#     printUsage
-#     echo "Error: elasticsearch-url parameter was required."
-#     exit 0
-# fi
+if [ -z "$ES_URL" ]; then
+    printUsage
+    echo "Error: elasticsearch-url parameter was required."
+    exit 0
+fi
 
+#
+# Default values for unassigned parameters
+#
 INTERVAL=${INTERVAL:-60}
+
+
+##----------------------------------------------------------------------##
 
 checkUtilitiesCommand
 
+#
 # Main loop to check offset periodically.
+#
 while : ; do
     for group in $@ ; do
         checkConsumerGroupOffset $group &
